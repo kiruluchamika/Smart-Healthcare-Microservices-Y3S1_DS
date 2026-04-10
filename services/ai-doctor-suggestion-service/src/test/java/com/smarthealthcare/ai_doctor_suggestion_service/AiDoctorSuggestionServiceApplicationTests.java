296b906beef87a57007194d63b3563ab64493d07
Kirulu chamika <kiruluchamika2002@gmail.com>
2026-04-10 21:00:46 +0530

    add ai-doctor-suggestion-service initial

---
+++ services/ai-doctor-suggestion-service/src/test/java/com/smarthealthcare/ai_doctor_suggestion_service/AiDoctorSuggestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthealthcare.ai_doctor_suggestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDoctorSuggestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
